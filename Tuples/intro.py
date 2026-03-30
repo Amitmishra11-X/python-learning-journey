c25f0fbc94574d95eeefaaa0d55f517d07cc9316
Amit Mishra <am0651465@gmail.com>
2026-03-31 03:07:25 +0530

    tuple

---
+++ Tuples/intro.py
@@ -0,0 +1,8 @@
+##Python refers to values that cannot change as immutable, and an immutable list is called a tuple 
+dimensions = (200, 50)
+print(dimensions)
+print(dimensions[0])
+print(dimensions[1])
+
+
+dimensions[0] = 250  ## change not possible 
