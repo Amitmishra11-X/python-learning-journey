0a046e718d873589df50d1bbd9239161d4b299f5
Amit Mishra <am0651465@gmail.com>
2026-03-31 23:40:37 +0530

    if statement

---
+++ if_statement/intro.py
@@ -0,0 +1,28 @@
+age = 18
+
+if age >= 18:
+    print("You are eligible to vote")
+
+number = 5
+
+if number % 2 == 0:
+    print("Even number")
+else:
+    print("Odd number")
+##Example 3: if-elif-else
+marks = 75
+
+if marks >= 90:
+    print("Grade A")
+elif marks >= 60:
+    print("Grade B")
+else:
+    print("Grade C")
+##🎯 Real-Life Example
+username = "admin"
+password = "1234"
+
+if username == "admin" and password == "1234":
+    print("Login successful")
+else:
+    print("Invalid credentials")
