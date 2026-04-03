36a36d49ce9665b784d0bc006ee746cbb4f221a7
Amit Mishra <am0651465@gmail.com>
2026-04-04 03:40:21 +0530

    modify values

--- Dictionaries/mutable_dic.py
+++ Dictionaries/mutable_dic.py
@@ -4,5 +4,16 @@ print(f"the alein is{alien_o['color']}")
 alien_o['color'] = 'yellow'
 print(f"the alein is{alien_o['color']}")
 # modifying values in a dictionary
-alien_o = {'x_position': 0, 'y_position': 25, 'speed': 'medium' }
 
+alien_o = {'x_position': 0, 'y_position': 25, 'speed': 'medium' }
+print(f"original position of x : {alien_o['x_position']}")
+if alien_o['speed']=='slow':
+    x_increment = 1
+elif alien_o['speed'] == 'medium':
+    x_increment = 2
+elif alien_o['speed'] == 'fast':
+    x_increment = 4
+else :
+    x_increment = 0 
+alien_o['x_position'] = alien_o['x_position'] + x_increment
+print(f"new position of x: {alien_o['x_position']}")
--- Dictionaries/tempCodeRunnerFile.py
+++ Dictionaries/tempCodeRunnerFile.py
@@ -1,6 +1,12 @@
-
-alien_o = {'color': 'green', 'points': 5}
-print(alien_o)
-alien_o['x-position'] = 0
-alien_o['y-position'] = 25
-print(alien_o)
+alien_o = {'x_position': 0, 'y_position': 25, 'speed': 'medium' }
+print(f"original position : {alien_o['x_position']}")
+if alien_o['speed']=='slow':
+    x_increment = 1
+elif alien_o['speed'] == 'medium':
+    x_increment = 2
+elif alien_o['speed'] == 'fast':
+    x_increment = 4
+else :
+    x_increment = 0 
+alien_o['x_position'] = alien_o['x_position'] + x_increment
+print(f"new position : {alien_o['x_position']}")
