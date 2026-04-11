e7610932ddbda0c8f0470c38c6a8e90ebf5b644b
Amit Mishra <am0651465@gmail.com>
2026-04-12 02:03:18 +0530

    input

---
+++ input/intro.py
@@ -0,0 +1,9 @@
+message = input("write anything that you to say : ")
+print(message)
+
+number = input("enter no. to define")
+number = int(number)
+if number % 2 == 0: 
+    print("number is even")
+else : 
+    print("odd")
---
+++ input/tempCodeRunnerFile.py
@@ -0,0 +1,7 @@
+
+number = input("enter no. to define")
+number = int(number)
+if number % 2 == 0: 
+    print("number is even")
+else : 
+    print("odd")
