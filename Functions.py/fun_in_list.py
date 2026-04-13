a1f997345652da1208006930c798e0ad08118475
Amit Mishra <am0651465@gmail.com>
2026-04-14 02:16:23 +0530

    new

--- Functions.py/fun_in_list.py
+++ Functions.py/fun_in_list.py
@@ -1,6 +1,6 @@
 def greet_users(names):
     for name in names:
-        msg = f"hello my friend {names}"
+        msg = f"hello my friend {name.title()}"
         print(msg)
         
 usernames = ['marie', 'sambinvi', 'kulwati' ]
--- Functions.py/return_values.py
+++ Functions.py/return_values.py
@@ -10,4 +10,20 @@ print(professor)
 professor = get_formatted_name('Dr.Arjun', 'rai', 'patel')
 print(professor)
 
-    
+""" RETURNING A DICTIONARY"""
+def build_person(first_name, last_name):
+    person = {'first': first_name, 'last': last_name}    
+    return person
+musician = build_person('jimi', 'hendrix')
+print(musician)
+
+"""Using a Function with a while loop """ """ infinite loop"""
+def get_formatted_name(first_name, last_name):
+    full_name = f"{first_name} {last_name}"
+    return full_name
+while True:
+    print('\nplease tell me your name : ')
+    f_name = input("First name:")
+    l_name = input("last name:")
+    formatted_name = get_formatted_name(f_name, l_name)
+    print(f"\nhello {formatted_name}")
