6e162ae0a59130d87f4729fccaebd6f36d0e23bb
Amit Mishra <am0651465@gmail.com>
2026-04-14 02:31:04 +0530

    function

--- Functions/fun_in_list.py
+++ Functions/fun_in_list.py
@@ -5,3 +5,15 @@ def greet_users(names):
         
 usernames = ['marie', 'sambinvi', 'kulwati' ]
 greet_users(usernames)
+
+"""MODIFYING A LIST IN FUNCTION"""
+
+unprinted_designs = ['phone case', 'robot pendent', 'dodecahedron']
+completed_models = []
+while unprinted_designs:
+    current_design = unprinted_designs.pop()
+    print(f"Printing model : {current_design}")
+    completed_models.append(current_design)
+print(f"\nthe following models have been printed : ")
+for completed_model in completed_models:
+    print(completed_model)
--- Functions/tempCodeRunnerFile.py
+++ Functions/tempCodeRunnerFile.py
@@ -1,7 +1,10 @@
-def greet_users(names):
-    for name in names:
-        msg = f"hello my friend {names}"
-        print(msg)
-        
-usernames = ['marie', 'sambinvi', 'kulwati' ]
-greet_users(usernames)
+
+unprinted_designs = ['phone case', 'robot pendent', 'dodecahedron']
+completed_models = []
+while unprinted_designs:
+    current_design = unprinted_designs.pop()
+    print(f"Printing model : {current_design}")
+    completed_models.append(current_design)
+print(f"\nthe following models have been printed : ")
+for completed_model in completed_models:
+    print(completed_model)
