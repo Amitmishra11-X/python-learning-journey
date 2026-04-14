20eeaccc165a27122b1e71e243e6b06d979b04a9
Amit Mishra <am0651465@gmail.com>
2026-04-15 00:49:49 +0530

    add

--- OOPS/car_class.py
+++ OOPS/car_class.py
@@ -15,3 +15,22 @@ my_car = Car('audi', 'a4', 2019)
 print(my_car.get_descriptive_name())
 
 """Setting a default for an attribute """
+class Car:
+    """A simple attempt to represent a car"""
+
+    def __init__(self, make, model, year):
+        self.make = make
+        self.model = model
+        self.year = year
+        self.odometer = 0
+
+    def get_descriptive_name(self):
+        long_name = f"{self.year} {self.make} {self.model}"
+        return long_name
+    def read_odometer(self):
+        print(f"this car has {self.odometer} miles on it!")
+
+
+my_car = Car('audi', 'a4', 2019)
+print(my_car.get_descriptive_name())
+my_car.read_odometer()
